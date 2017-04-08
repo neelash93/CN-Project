df5cbfb7d43544dc0a9961dc0f07e5c8f76e6591
Prateek Joshi <prateekjoshi2013@gmail.com>
2017-04-08 17:07:39 -0400

    created server threads and made changes to message builder

--- src/ServerRequestHandler.java
+++ src/ServerRequestHandler.java
@@ -1,73 +1,125 @@
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.util.concurrent.locks.ReentrantLock;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashMap;
 
-class ServerRequestHandler implements Runnable{
-	private String message;    //message received from the client
-	private String MESSAGE;    //uppercase message send to the client
-	private Socket connection;
-	private ObjectInputStream in;	//stream read from the socket
-	private ObjectOutputStream out;    //stream write to the socket
-	private String peerName;		//The index number of the client
+	 class ServerRequestHandler implements Runnable {
 
-	public ServerRequestHandler(Socket connection, String peerName) {
-		this.connection = connection;
-		this.peerName = peerName;
-	}
-	
-	
 
-	public void run() {
-		try{
-			//initialize Input and Output streams
-			out = new ObjectOutputStream(connection.getOutputStream());
-			out.flush();
-			in = new ObjectInputStream(connection.getInputStream());
+		private Message message;    //message received from the client
+		private Socket connection;
+		private ObjectInputStream in;   //stream read from the socket
+		private int clientId;     //The index number of the client
+        private List<Message>receivedMessages;
+		ReentrantLock lock;
+		private static final HashMap<Integer, MessageType> intToMessageType;
+
+		static {
+			intToMessageType = new HashMap<Integer, MessageType>();
+			for (MessageType type : MessageType.values()) {
+				intToMessageType.put(type.getValue(), type);
+			}
+		}
+
+
+		public ServerRequestHandler(Socket connection, int clientId,List<Message>receivedMessages) {
+			this.connection = connection;
+			this.clientId = clientId;
+			this.receivedMessages=receivedMessages;
+			this.lock=new ReentrantLock();
+
+		}
+
+		public void run() {
 			try{
-				while(true)
-				{	System.out.println("fgdfgdfgd");
-					//receive the message sent from the client
-					message = (String)in.readObject();
-					//show the message to the user
-					System.out.println("Receive message: " + message + " from client " + peerName);
-					//Capitalize all letters in the message
-					MESSAGE = message.toUpperCase();
-					//send MESSAGE back to the client
-					sendMessage(MESSAGE);
+				//initialize Input and Output streams
+				in = new ObjectInputStream(connection.getInputStream());
+
+				try{
+
+					while(true)
+					{
+						//receive the message sent from the client
+						byte[] temp = (byte[])in.readObject();
+                        /*for (int i = 0; i < temp.length; i++) {
+                            System.out.println(i + " : " + temp[i]);
+                        }*/
+						message = convertToMessage(temp, clientId);
+						//System.out.println("Placing message in array: " + message.toString());
+
+
+						try{
+							lock.lock();
+							receivedMessages.add(message);
+
+						}catch(Exception e){
+							System.out.println("failed to add message to the messageList");
+						}finally {
+							lock.unlock();
+						}
+
+						//show the message to the user
+						//System.out.println("Received message: " + message.toString() + " from client " + clientId);
+                        /*
+                         *
+                         * TODO: Add this to the logger somehow
+                         *
+                         */
+					}
+				}
+				catch(ClassNotFoundException e){
+					e.printStackTrace();
+					System.out.println("Data received in unknown format");
 				}
 			}
-			catch(ClassNotFoundException classnot){
-				System.err.println("Data received in unknown format");
+			catch(IOException e){
+				e.printStackTrace();
+				System.out.println("Disconnect with Client " + clientId);
+			}
+			finally{
+
+				try{
+					in.close();
+					connection.close();
+				}
+				catch(IOException e){
+					System.out.println(e);
+				}
 			}
 		}
-		catch(IOException ioException){
-			System.out.println("Disconnect with Client " + peerName);
-		}
-		finally{
-			//Close connections
-			try{
-				in.close();
-				out.close();
-				connection.close();
+
+
+		private Message convertToMessage(byte[] data, int clientID) {
+			//Check for handshake:
+
+			if (data.length >= 18) {
+				byte[] bytes = Arrays.copyOfRange(data, 0, 18);
+				String header = new String(bytes, Charset.forName("UTF-8"));
+				if (header.equals("P2PFILESHARINGPROJ")) {
+					int newPeerID = ByteBuffer.allocate(4).put(Arrays.copyOfRange(data, 28, 32)).getInt(0);
+					return new Message(newPeerID, MessageType.HANDSHAKE, null, clientID);
+				}
 			}
-			catch(IOException ioException){
-				System.out.println("Disconnect with Client " + peerName);
-			}
+
+			//Converts the incoming bytes into the actual Message with its associated clientId
+			int length = ByteBuffer.allocate(4).put(Arrays.copyOfRange(data, 0, 4)).getInt(0);
+			byte[] payload = Arrays.copyOfRange(data, 5, data.length);
+
+
+
+			return new Message(length, fromInt(data[4]), payload, clientID);
 		}
+
+		public static MessageType fromInt(int i) {
+			MessageType type = intToMessageType.get(Integer.valueOf(i));
+			return type;
+		}
+
 	}
 
-	//send a message to the output stream
-	public void sendMessage(String msg)
-	{
-		try{
-			out.writeObject(msg);
-			out.flush();
-			System.out.println("Send message: " + msg + " to Client " + peerName);
-		}
-		catch(IOException ioException){
-			ioException.printStackTrace();
-		}
-	}
 
-}
